ebbff100edcc59b7b9fce3e0d7e1a72f7286028d
Rafael Vastag <rgvastag@gmail.com>
2021-09-28 15:08:25 -0300

    creating directory

---
+++ Directory/Directory/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\rgvas\HOME";
+
+            try
+            {
+                DirectoryInfo direc = new DirectoryInfo(path);
+                var folders = direc.EnumerateDirectories();
+
+                Console.WriteLine("FOLDERS: ");
+
+                foreach (var s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                var files = direc.EnumerateFiles();
+
+                Console.WriteLine("FILES: ");
+
+                foreach (var s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                direc.CreateSubdirectory(path + @"\newfolder");
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
