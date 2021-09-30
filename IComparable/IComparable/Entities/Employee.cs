3407f5e78843d3e4abd3cfa5e741b5eda9039073
Rafael Vastag <rgvastag@gmail.com>
2021-09-29 21:36:47 -0300

    IComparable

---
+++ IComparable/IComparable/Entities/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System;
+namespace Entities
+{
+    class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csv)
+        {
+            string[] vect = csv.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing error: argument is not an employee");
+            }
+
+            Employee e = obj as Employee;
+            return Name.CompareTo(e.Name);
+        }
+    }
+}
---
+++ IComparable/IComparable/Program.cs
@@ -0,0 +1,37 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IComparableExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"C:\Users\rgvas\HOME\file1.txt";
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List <Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+
+                    foreach (Employee item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message); ;
+            }
+        }
+    }
+}
