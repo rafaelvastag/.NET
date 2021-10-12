e6352c238136c5a5429822e280347913bd49d688
Rafael Vastag <rgvastag@gmail.com>
2021-10-12 12:04:53 -0300

    generics restrictions

---
+++ LogProgram/LogProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Entities;
+namespace LogProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<LogRecord> hashSet = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        hashSet.Add(new LogRecord { Username = name, Instant = instant });
+                    }
+                    Console.WriteLine("Total Users: " + hashSet.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
